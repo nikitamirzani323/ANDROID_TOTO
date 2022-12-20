f955c12e1cec2ee0c65bb52b71c16b480065e137
Devil <81461889+nikitamirzani323@users.noreply.github.com>
2022-12-20 16:00:12 +0700

    New Code + Logo

---
+++ app/src/main/java/com/toto/sdsb/MainActivity.java
@@ -0,0 +1,32 @@
+package com.toto.sdsb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnNext;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+
+        btnNext = findViewById(R.id.btnNext);
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Home.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
