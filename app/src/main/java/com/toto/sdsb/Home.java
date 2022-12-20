1165cc0d1ddef07230b907a0a2504d39923c3c55
Devil <81461889+nikitamirzani323@users.noreply.github.com>
2022-12-20 17:45:44 +0700

    update home

--- app/src/main/java/com/toto/sdsb/Home.java
+++ app/src/main/java/com/toto/sdsb/Home.java
@@ -2,13 +2,29 @@ package com.toto.sdsb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
-
+    private Button btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_home);
+
+        btnBack = findViewById(R.id.btnBack);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
